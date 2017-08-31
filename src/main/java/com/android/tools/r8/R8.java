5454c97f94eb8a7b26b4dd87464e3f35189dd174
Rico Wind <ricow@google.com>
2017-08-31 10:24:08 +0200

    Version 0.1.7.
    Test only changes, no need for cherry pick
    
    Merge: Add archive script that the bots can archive release versions
    CL: https://r8-review.googlesource.com/#/c/r8/+/5340/
    
    Merge: Fix archiving bucket
    CL: https://r8-review.googlesource.com/#/c/r8/+/5441/
    Change-Id: Iff958572bf4cfe1b363ed09939e05d14de8ff0bb

--- src/main/java/com/android/tools/r8/R8.java
+++ src/main/java/com/android/tools/r8/R8.java
@@ -71,7 +71,7 @@ import java.util.concurrent.Executors;
 
 public class R8 {
 
-  private static final String VERSION = "v0.1.6";
+  private static final String VERSION = "v0.1.7";
   private final Timing timing = new Timing("R8");
   private final InternalOptions options;
 
