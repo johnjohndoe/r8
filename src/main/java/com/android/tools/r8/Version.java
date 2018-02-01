d2dce2d49cec832cc9e4bbd5fb4841b79eb60b44
Mads Ager <ager@google.com>
2018-02-01 12:36:49 +0100

    Version 1.0.8.
    Merge: Fix types when translating dup2_x2 from the jar frontend.
    CL: https://r8-review.googlesource.com/15761
    
    Merge: Add regression test for dup2_x2 bug.
    CL: https://r8-review.googlesource.com/15762
    
    R=zerny@google.com
    
    Change-Id: I49b8d7b2996819e7b89a92d5d621a567f812e19c

--- src/main/java/com/android/tools/r8/Version.java
+++ src/main/java/com/android/tools/r8/Version.java
@@ -11,7 +11,7 @@ public final class Version {
 
   // This field is accessed from release scripts using simple pattern matching.
   // Therefore, changing this field could break our release scripts.
-  public static final String LABEL = "v1.0.7";
+  public static final String LABEL = "v1.0.8";
 
   private Version() {
   }
