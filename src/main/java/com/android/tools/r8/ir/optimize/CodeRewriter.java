47e3bda6acd15806fe0e5cdb88721f2b680a8479
Jinseong Jeon <jsjeon@google.com>
2018-06-04 12:35:28 -0700

    Version 1.0.31
    Merge: Fix an infinite loop in CodeRewriter#simplifyArrayConstruction.
    CL: https://r8-review.googlesource.com/c/r8/+/21880
    
    Bug: 87341268
    Change-Id: Ibccf02e65171eec8c4aa1c513fe0f65f11f23311

--- src/main/java/com/android/tools/r8/ir/optimize/CodeRewriter.java
+++ src/main/java/com/android/tools/r8/ir/optimize/CodeRewriter.java
@@ -1376,10 +1376,8 @@ public class CodeRewriter {
           return values;
         }
       }
-      block =
-          block.exit().isGoto() && !visitedBlocks.contains(block.exit().asGoto().getTarget())
-              ? block.exit().asGoto().getTarget()
-              : null;
+      BasicBlock nextBlock = block.exit().isGoto() ? block.exit().asGoto().getTarget() : null;
+      block = nextBlock != null && !visitedBlocks.contains(nextBlock) ? nextBlock : null;
       it = block != null ? block.listIterator() : null;
     } while (it != null);
     return null;
@@ -1470,7 +1468,9 @@ public class CodeRewriter {
       // Second pass: remove all the array put instructions for the array for which we have
       // inserted a fill array data instruction instead.
       if (!storesToRemoveForArray.isEmpty()) {
+        Set<BasicBlock> visitedBlocks = Sets.newIdentityHashSet();
         do {
+          visitedBlocks.add(block);
           it = block.listIterator();
           while (it.hasNext()) {
             Instruction instruction = it.next();
@@ -1492,7 +1492,8 @@ public class CodeRewriter {
               }
             }
           }
-          block = block.exit().isGoto() ? block.exit().asGoto().getTarget() : null;
+          BasicBlock nextBlock = block.exit().isGoto() ? block.exit().asGoto().getTarget() : null;
+          block = nextBlock != null && !visitedBlocks.contains(nextBlock) ? nextBlock : null;
         } while (block != null);
       }
     }
